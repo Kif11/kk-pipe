5fb5acd01c5e8c5ab33622e00b3f7409453b1c32
Kif <kovalewskiy@gmail.com>
2014-12-09 00:36:10 -0800

    laptop

--- 123.py
+++ 123.py
@@ -1,5 +1,9 @@
 import hrpyc
 from lib_vendor import yaml
+import sys
+sys.path.append('F:/python/kk_pipe/lib_vendor')
+import rpyc
+import rpyc.servers.classic_server
 
 # Run rpc server
 server = hrpyc.start_server()
@@ -16,4 +20,15 @@ def set_global_var(name, value):
 
 set_global_var("JOB", hou_session_dictionary["JOB"])
 set_global_var("WORK", hou_session_dictionary["WORK"])
+
+# Connect ot maya rpc
+def import_remote_module(server="127.0.0.1", port=45454, name="maya.cmds"):
+    connection = rpyc.classic.connect(server, port)
+    return connection, connection.modules[name]
+
+try:
+    connection, cmds = import_remote_module()
+except:
+    pass
+    print "Can not connect to Maya rpc server"
 
--- userSetup.py
+++ userSetup.py
@@ -1,6 +1,44 @@
 import maya.cmds as cmds
 import maya.utils as util
+import sys
+sys.path.append('./lib_vendor')
+import threading
+import rpyc
+import rpyc.servers.classic_server
+
+# Start Maya rpc server
+def start_server(port=45454, use_thread=True, quiet=True):
+    # Note that quiet=False only applies when use_thread=False.
+    if use_thread:
+        thread = threading.Thread(
+            target=lambda: start_server(port, use_thread=False))
+        thread.start()
+        return thread
+
+    args = []
+    if quiet:
+        args.append("-q")
+    args.extend(("-p", str(port), "--dont-register"))
+
+    options, args = rpyc.servers.classic_server.parser.parse_args(args)
+    options.registrar = None
+    options.authenticator = None
+    rpyc.servers.classic_server.serve_threaded(options)
+
+server = start_server()
+
+# Connect to Houdini rpc server
+def import_remote_module(server="127.0.0.1", port=18811, name="hou"):
+    connection = rpyc.classic.connect(server, port)
+    return connection, connection.modules[name]
+
+try:
+    connection, hou = import_remote_module()
+except:
+    print "Can not connect to Houdini rpc server"
+    pass
+
+
 
 util.executeDeferred(cmds.polyCube)
-
 print "userSetup.py has been executed"
